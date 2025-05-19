fa98ebc00bf58b76624dffc152cbafa00a536fdb
Sporkshirts <sporkshirts@gmail.com>
2025-05-19 18:25:50 -0400

    Better tether to flesh, but 2nd won't attach to brain.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -17,12 +17,20 @@ public class CollisionHandler : MonoBehaviour
             Debug.Log("It's 1.");
 
             nerveSpawn.Spawn2(flesh1);
+            flesh1.transform.position = flesh1.transform.position + new Vector3(2.11f, 0.33f, 0.82f);
+            Rigidbody rb = flesh1.GetComponent<Rigidbody>();
+            rb.constraints = RigidbodyConstraints.FreezeAll;
 
             // update "score that Flesh1 is now tethered.
         }
         else if (other.name == "Flesh2")
         {
             Debug.Log("It's 2.");
+
+            nerveSpawn.Spawn2(flesh2);
+            flesh2.transform.position = flesh2.transform.position + new Vector3(-4.8f, 2.3f, 1.82f);
+            Rigidbody rb = flesh2.GetComponent<Rigidbody>();
+            rb.constraints = RigidbodyConstraints.FreezeAll;
             // update "score that Flesh1 is now tethered.
         }
 
