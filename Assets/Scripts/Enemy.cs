e0f3ca240e0c1581c90d9d851b12b4f553add63a
Reaveeee <reavewt@gmail.com>
2025-05-19 20:06:05 +0200

    Improved Enemy Ai
    - Enemies will only detect you if they have line of sight
    
    - Enemies will check your latest position after losing line of sight

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,7 +5,12 @@ public class Enemy : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private float speed = 3.0f;
     [SerializeField] private bool chase = false;
+    [SerializeField] private bool returnToSpawn = false;
+    [SerializeField] private float searchRange;
+    [SerializeField] private LayerMask searchMask;
+
     Vector3 spawnPosition;
+    Vector3 lastTargetPosition;
     Rigidbody rb;
 
     private void Start()
@@ -16,25 +21,25 @@ public class Enemy : MonoBehaviour
 
     void FixedUpdate()
     {
+        chase = SearchForPlayer();
         if (!chase)
         {
-            if (Vector3.Distance(transform.position, spawnPosition) > 0.2f)
+            if(returnToSpawn)
             {
-                RotateTowardsDestination(spawnPosition);
-                Vector3 direction = (spawnPosition - transform.position).normalized;
-                Vector3 newPosition = rb.position + direction * speed * Time.fixedDeltaTime;
-                rb.MovePosition(newPosition);
+                if (Vector3.Distance(transform.position, spawnPosition) > 0.2f) MoveToDestination(spawnPosition);
             }
-
+            else if (Vector3.Distance(transform.position, lastTargetPosition) > 0.2f)
+            {
+                if (Vector3.Distance(transform.position, lastTargetPosition) > 0.2f) MoveToDestination(lastTargetPosition);
+                if(Vector3.Distance(transform.position, lastTargetPosition) < 0.3f) returnToSpawn = true;
+            }
         }
         else
         {
-            Vector3 direction = (player.position - transform.position).normalized;
-            Vector3 newPosition = rb.position + direction * speed * Time.fixedDeltaTime;
-            rb.MovePosition(newPosition);
-            RotateTowardsDestination(player.position);
+            returnToSpawn = false;
+            MoveToDestination(player.position);
+            lastTargetPosition = player.position;
         }
-
     }
 
     private void RotateTowardsDestination(Vector3 destination)
@@ -47,7 +52,26 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void MoveToDestination(Vector3 destination)
+    {
+        RotateTowardsDestination(destination);
+        Vector3 direction = (destination - transform.position).normalized;
+        Vector3 newPosition = rb.position + direction * speed * Time.fixedDeltaTime;
+        rb.MovePosition(newPosition);
+    }
+
+    private bool SearchForPlayer()
+    {
+        Ray ray = new Ray(transform.position, (player.position - transform.position).normalized);
+        if(Physics.Raycast(ray, out RaycastHit hitInfo, searchRange, searchMask))
+        {
+            if (hitInfo.collider.CompareTag("Player")) return true;
+        }
+        return false;
+    }
+
+    //Old Search Method
+    /*private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
@@ -61,5 +85,5 @@ public class Enemy : MonoBehaviour
         {
             chase = false;
         }
-    }
+    }*/
 }
